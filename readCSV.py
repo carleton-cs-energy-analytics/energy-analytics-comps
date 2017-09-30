d4509d9e2fb0aeee4d0195a43d9796b7ad9eefe9
Carolyn Ryan <ryanc2@carleton.edu>
2017-09-30 14:24:59 -0500

    Dataframe in method now, rearranged sys logic a little

--- readCSV.py
+++ readCSV.py
@@ -9,30 +9,22 @@ import pandas as pd
 def main():
     if (len(sys.argv) > 1):
         # CASE: transform the CSV from the bad input to the better one
-        if sys.argv[1] = "transform":
+        if sys.argv[1] == "transform":
             fileName = sys.argv[2]
+            betterFileName = "better" + fileName
             with open(fileName, 'r') as f:
                 reader = csv.reader(f)
                 dataList = list(reader)
             
             transfromCSVFromBadFormatToBetterFormat(dataList, betterFileName)
-    
+        else:
+            fileName = sys.argv[1]
+            get_dataframe_from_csv(fileName)
+
     # CASE: everything else we are trying to do.
     else:
-       betterFileName = "betterTestCSV.csv"
-       df = pd.read_csv(betterFileName, dtype=object)
-
-	    # Potential example of finding cycling?
-	    previous_entry = None
-	    cycle_count = 0
-	    for entry in df._series['ACDIN.VAV026:HEAT.COOL']:
-	        if entry != previous_entry and previous_entry != None:
-	            cycle_count += 1
-	        previous_entry = entry
-	
-	    for index, row in df.iterrows():
-	        # print(row)
-	        pass
+        # No Sys args, just use test file
+        get_dataframe_from_csv("betterTestCSV.csv") # Need to run transform first to get this file
 
 # takes in dataList, which represents the original CSV, and writes a new
 # CSV file that has mapped all the point with their name.
@@ -57,6 +49,22 @@ def transfromCSVFromBadFormatToBetterFormat(dataList, newCSVName):
         # Write the rest of the lines:
         for i in range(split + 2, len(dataList), 1):
             writer.writerow(dataList[i])
+
+def get_dataframe_from_csv(fileName):
+    df = pd.read_csv(fileName, dtype=object)
+
+    # Potential example of finding cycling?
+    previous_entry = None
+    cycle_count = 0
+    for entry in df._series['ACDIN.VAV026:HEAT.COOL']:
+        if entry != previous_entry and previous_entry != None:
+            cycle_count += 1
+        previous_entry = entry
+    print("{} cycles(?) in this example".format(cycle_count))
+
+    for index, row in df.iterrows():
+        # print(row)
+        pass
         
 if __name__ == "__main__":
     main()

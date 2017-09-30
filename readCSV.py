aae41f50d3ca6748e97b7063be7b21b2c99bf3b9
zephyrLucas <zephyr@lucas.net>
2017-09-30 13:42:20 -0500

    readCSV now calls function, and fixed some bugs.  It runs on TestCSV\!

--- readCSV.py
+++ readCSV.py
@@ -2,13 +2,13 @@ import csv
 import sys
 
 def main():
-    with open(sys.argv[1], 'r') as f:
+    fileName = sys.argv[1]
+    with open(fileName, 'r') as f:
         reader = csv.reader(f)
         dataList = list(reader)
     
     # dataList now is a 2x2 matix representing the CSV.
-    
-    transfromCSVFromBadFormatToBetterFormat
+    transfromCSVFromBadFormatToBetterFormat(dataList, "better" + fileName + ".csv");
 
 # takes in dataList, which represents the original CSV, and writes a new
 # CSV file that has mapped all the point with their name.
@@ -17,7 +17,7 @@ def transfromCSVFromBadFormatToBetterFormat(dataList, newCSVName):
     split = 0
     pointToName = {}
     while (dataList[split][0] != ""):
-        pointToName[dataList[split][0]] = dataList[split][1]
+        pointToName[(dataList[split][0])[:-1]] = dataList[split][1]
         split += 1
     
     # Write the new data to a CSV:
@@ -26,12 +26,12 @@ def transfromCSVFromBadFormatToBetterFormat(dataList, newCSVName):
         
         # Construct and write the firstline:
         firstLine = [dataList[split + 1][0], dataList[split + 1][1]]
-        for j in xrange(2, len(dataList[split + 1], 1)):
-            firstLine.append(pointToName[dataList[split + 1][j]]):
+        for j in range(2, len(dataList[split + 1]), 1):
+            firstLine.append(pointToName[dataList[split + 1][j]])
         writer.writerow(firstLine)
         
         # Write the rest of the lines:
-        for i in xrange(split + 2, len(dataList), 1):
+        for i in range(split + 2, len(dataList), 1):
             writer.writerow(dataList[i])
         
 if __name__ == "__main__":

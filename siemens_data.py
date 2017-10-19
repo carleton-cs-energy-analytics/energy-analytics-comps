a85365c5127abf1ac897c268a9d3dbf863529f18
Kiya Govek <kiyagovek@eduroam-173-128.dyn.carleton.edu>
2017-10-18 19:25:03 -0500

    Deleted duplicate siemens_data file

--- siemens_data.py
+++ siemens_data.py
@@ -1,74 +1,79 @@
-import csv
-import sys
-import pandas as pd
 from datetime import datetime
+import pandas as pd
 
-""" Class for reading and manipulating Siemens data from transformed CSVs. 
-	Contains API for accessing data, as well as function for reading the CSV's. 
-"""
 
 class SiemensData:
+    """ Class for reading and manipulating Siemens data from transformed CSVs.
+            Contains API for accessing data, as well as function for reading the CSV's.
+    """
 
-	data = None
-	datetimes = []
+    def __init__(self):
+        self.data = None  # Pandas DataFrame created from transformed CSV.
 
-	# Open and store transformed CSV data as class pd object. 
-	def read_csv(self, filename): 
-		self.data = pd.read_csv(filename, dtype=object)
+        """ List of datetime objects created from timestamps in CSV, used for
+            pairing data in the spreadsheet with timestamps without looking
+            through timestamps each time. """
+        self.datetimes = []
 
-	def find_data_for_room(self, room_name):
-		equipment_dictionary = {} #Key = equipment name, value = sorted list of tuples containing (timestamp, data)
+    # Open and store transformed CSV data as class pd object.
+    def read_csv(self, filename):
+        self.data = pd.read_csv(filename, dtype=object)
 
-		if self.data is None:
-			raise Exception("You haven't read in a csv yet!")
+    def find_data_for_room(self, room_name):
+        # Key = equipment name, value = sorted list of tuples containing
+        # (timestamp, data)
+        equipment_dictionary = {}
 
-		for header in self.data:
-			if room_name in header:
-				equipment_data = [] # Initialize container for (timestamp, data) tuples
+        if self.data is None:
+            raise Exception("You haven't read in a csv yet!")
 
-				equipment_name = self.find_equipment_name(header) #Key for dictionary
-				if len(self.datetimes) < 1: 
-					raise Exception("You haven't parsed the timestamps yet!")
+        for header in self.data:
+            if room_name in header:
+                # Initialize container for (timestamp, data) tuples
+                equipment_data = []
 
-				for i in range(len(self.datetimes)):
-					datetime = self.datetimes[i]
-					data = self.data.get_value(i, header)
-					equipment_data.append((datetime, data))
+                equipment_name = self.find_equipment_name(
+                    header)  # Key for dictionary
+                if len(self.datetimes) < 1:
+                    raise Exception("You haven't parsed the timestamps yet!")
 
-				equipment_dictionary[equipment_name] = equipment_data
+                for i in range(len(self.datetimes)):
+                    date_time = self.datetimes[i]
+                    data = self.data.get_value(i, header)
+                    equipment_data.append((date_time, data))
 
-		return equipment_dictionary
+                equipment_dictionary[equipment_name] = equipment_data
 
+        return equipment_dictionary
 
+    def find_equipment_name(self, header):
+        parts_of_header = header.split(".")
+        return parts_of_header[-1]
 
-	def find_equipment_name(self, header):
-		parts_of_header = header.split(".")
-		return parts_of_header[-1]
+    def create_datetimes_list(self):
+        datetimes = []
+        for i in range(
+                len(self.data.columns.values)):  # Iterate through all rows
+            # Get date and time from particular column
+            date = self.data.get_value(i, "<>Date")
+            time = self.data.get_value(i, "Time")
 
-	def create_datetimes_list(self):
-		datetimes = []
-		for i in range(len(self.data.columns.values)): # Iterate through all rows
-			# Get date and time from particular column
-			date = self.data.get_value(i, "<>Date")
-			time = self.data.get_value(i, "Time")
+            timestamp = date + " " + time
+            # create datetime object from MM/DD/YY xx:xx:xx format
+            datetime_object = datetime.strptime(timestamp, "%m/%d/%y %H:%M:%S")
+            datetimes.append(datetime_object)
 
-			timestamp = date + " " + time
-			datetime_object = datetime.strptime(timestamp, "%m/%d/%y %H:%M:%S") #create datetime object from MM/DD/YY xx:xx:xx format
-			datetimes.append(datetime_object)
-
-		self.datetimes = datetimes
-
+        self.datetimes = datetimes
 
 
 def main():
-	filename = "betterTestCSV.csv"
-	data_reader = SiemensData()
-	data_reader.read_csv(filename)
-	data_reader.create_datetimes_list()
-	equipment_dictionary = data_reader.find_data_for_room("ACDIN.AH1")
-	print(equipment_dictionary)
-
+    filename = "betterTestCSV.csv"
+    data_reader = SiemensData()
+    data_reader.read_csv(filename)
+    data_reader.create_datetimes_list()
+    equipment_dictionary = data_reader.find_data_for_room("ACDIN.AH1")
+    print(equipment_dictionary)
 
 
 if __name__ == '__main__':
-	main()
+    main()

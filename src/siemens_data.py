700822f5a251ce5cd7f51663c1d1df0f20886f67
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-22 20:25:51 -0500

    Test agina

--- src/siemens_data.py
+++ src/siemens_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 class SiemensData:
     """ Class for reading and manipulating Siemens data from transformed CSVs.
             Contains API for accessing data, as well as function for reading the CSV's.
-            TEST
+            TEST222
     """
 
     def __init__(self):

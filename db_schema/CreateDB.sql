21ddcd18152434fa91d73a5555ad465f6a407ec8
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-29 20:07:19 -0500

    Create DB sql code, this was auto generated so idk if its what we will want

---
+++ db_schema/CreateDB.sql
@@ -0,0 +1,12 @@
+-- Database: carleton_energy_round1
+
+-- DROP DATABASE carleton_energy_round1;
+
+CREATE DATABASE carleton_energy_round1
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;

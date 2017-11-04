0b86c12551910bb4b324f90850c2b34a80b40ba4
Kiya Govek <kiyagovek@eduroam-173-42.dyn.carleton.edu>
2017-11-04 13:36:09 -0500

    changed equipment to point

--- src/add_points.py
+++ src/add_points.py
@@ -9,7 +9,7 @@ json_dict = json.load("magic")
 db_connection = psycopg2.connect("magic")
 db = db_connection.cursor()
 
-class EquipmentType:
+class PointType:
     name = ""
     return_type = ""
     units = None  # might stay undefined if not a numerical point
@@ -21,15 +21,15 @@ class EquipmentType:
 
     def add_self_to_database(self): # possibly get passed connection to db
         if self.return_type == "enumerated":
-            db.execute("""INSERT INTO EquipmentTypes (Name, Units, ReturnType)
+            db.execute("""INSERT INTO PointTypes (Name, Units, ReturnType)
                                     VALUES (%s, %s, %s);""", (self.name, ",".join(self.enumeration_settings),
                                                               self.return_type))
         else:
-            db.execute("""INSERT INTO EquipmentTypes (Name, Units, ReturnType)
+            db.execute("""INSERT INTO PointTypes (Name, Units, ReturnType)
                         VALUES (%s, %s, %s);""", (self.name, self.units, self.return_type))
 
     def get_id_from_database(self):
-        db.execute("SELECT * FROM EquipmentTypes WHERE Name = %s;", (self.name))
+        db.execute("SELECT * FROM PointTypes WHERE Name = %s;", (self.name))
         return db.fetchone()
 
 
@@ -46,11 +46,11 @@ def get_known_types():
     Gets all types currently in the database table
     """
     known_types = {}
-    db.execute("SELECT * FROM EquipmentTypes")
+    db.execute("SELECT * FROM PointTypes")
     next_entry = db.fetchone()
     while next_entry is not None:
         return_type = next_entry[2]
-        this_type = EquipmentType(next_entry[0], return_type)
+        this_type = PointType(next_entry[0], return_type)
         if return_type == "enumerated":
             this_type.enumeration_settings = next_entry[1].split(",")
         else:
@@ -60,9 +60,9 @@ def get_known_types():
     return known_types
 
 
-def get_equipment_type(point_name):
+def get_point_type(point_name):
     """
-    Returns the equipment type for the given name
+    Returns the point type for the given name
     If from the known type codes, name will be reasonable
     If new type, name will be a concatenation of return_type and units/enumeration_settings
     """
@@ -75,13 +75,13 @@ def get_equipment_type(point_name):
         return_type = point_dict["Analog Representation"]
         units = point_dict["Engineering Units"]
         type_name = return_type + units
-        new_type = EquipmentType(type_name, return_type)
+        new_type = PointType(type_name, return_type)
         new_type.units = units
     else:
         return_type = "enumerated"
         enumeration_settings = point_dict["Text Table"][1]
         type_name = return_type + ",".join(enumeration_settings)
-        new_type = EquipmentType(type_name, return_type)
+        new_type = PointType(type_name, return_type)
         new_type.enumeration_settings = enumeration_settings
     new_type.add_self_to_database()
     known_types[new_type.name] = new_type
@@ -90,15 +90,15 @@ def get_equipment_type(point_name):
 
 def add_point_to_db(point_name, room_id, point_source_id):
     """
-    Given some information about an equipment, gets the type and description and pushes that information to the database
+    Given some information about a point, gets the type and description and pushes that information to the database
     """
-    db.execute("SELECT * FROM EquipmentTypes WHERE Name = %s AND RoomID = %s;", (point_name, room_id))
+    db.execute("SELECT * FROM PointTypes WHERE Name = %s AND RoomID = %s;", (point_name, room_id))
     if db.fetchone() is not None:
         return # this point is already in database, don't add again
-    equip_type = get_equipment_type(point_name)
+    point_type = get_point_type(point_name)
     description = json_dict[point_name]["Descriptor"]
-    db.execute("""INSERT INTO Points (Name, RoomID, EquipmentTypeID, PointSourceID, Description)
-                VALUES (%s, %s, %s, %s, %s);""", (point_name, room_id, equip_type.get_id_from_database(),
+    db.execute("""INSERT INTO Points (Name, RoomID, PointTypeID, PointSourceID, Description)
+                VALUES (%s, %s, %s, %s, %s);""", (point_name, room_id, point_type.get_id_from_database(),
                                                   point_source_id, description))
 
 

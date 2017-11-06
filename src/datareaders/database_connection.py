aa08bf03e3907ebeceb62dd0d05c15af7e057278
Kiya Govek <kiyagovek@eduroam-173-42.dyn.carleton.edu>
2017-11-05 19:32:04 -0600

    camel case to underscore

--- src/datareaders/database_connection.py
+++ src/datareaders/database_connection.py
@@ -12,60 +12,60 @@ class DatabaseConnection:
 
     # add methods add object to database
 
-    def addBuilding(self, name):
+    def add_building(self, name):
         self.db.execute("INSERT INTO Buildings(Name) VALUES (%s);", (name))
 
-    def addRoom(self, name, building_name):
-        building_id = self.getIDBuilding(building_name)
+    def add_room(self, name, building_name):
+        building_id = self.get_building_id(building_name)
         self.db.execute("INSERT INTO Rooms(Name, BuildingID) VALUES (%s, %i);", (name, building_id))
 
-    def addPointType(self, point_type):
+    def add_point_type(self, point_type):
         self.db.execute("INSERT INTO PointTypes(Name, Units, ReturnType, Factor) VALUES (%s, %s, %s, %i);",
                         (point_type.name, point_type.get_units_placeholder(), point_type.return_type,
                          point_type.factor))
 
-    def addPoint(self, point):
-        room_id = self.getIDRoom(point.room, point.building)
-        type_id = self.getIDPointType(point.point_type)
+    def add_point(self, point):
+        room_id = self.get_room_id(point.room, point.building)
+        type_id = self.get_point_type_id(point.point_type)
         self.db.execute("INSERT INTO Points(Name, RoomID, PoinTypeID, PointSourceID, Description) VALUES (%s, %i, "
                         "%i, $i, $s);", (point.name, room_id, type_id, point.source, point.description))
 
-    def addPointValue(self, timestamp, point, value):
-        point_id = self.getIDPoint(point)
+    def add_point_value(self, timestamp, point, value):
+        point_id = self.get_point_id(point)
         self.db.execute("INSERT INTO PointValues (PointTimestamp, PointID, PointValue) VALUES (%s, %i, %i);",
                         (timestamp, point_id, value))
 
-    # getID methods return the ID of an object if it is in the database, or None if not
+    # get_-_id methods return the ID of an object if it is in the database, or None if not
 
-    def getIDBuilding(self, name):
+    def get_building_id(self, name):
         pass
 
-    def getIDRoom(self, name, building_id):
+    def get_room_id(self, name, building_id):
         pass
 
-    def getIDPointType(self, point_type):
+    def get_point_type_id(self, point_type):
         pass
 
-    def getIDPoint(self, point): # only need to use name and room/building combo
+    def get_point_id(self, point): # only need to use name and room/building combo
         pass
 
     # addUnique methods add object to database only if it is not already in the database
 
-    def addUniqueBuilding(self, name):
+    def add_unique_building(self, name):
         pass
 
-    def addUniqueRoom(self, name, building_name):
+    def add_unique_room(self, name, building_name):
         pass
 
-    def addUniquePointType(self, point_type):
+    def add_unique_point_type(self, point_type):
         pass
 
-    def addUniquePoint(self, point):
+    def add_unique_point(self, point):
         pass
 
     # getAll methods select * from database and return as a dictionary with key as name
 
-    def getAllPointTypes(self):
+    def get_all_point_types(self):
         known_types = {}
         self.db.execute("SELECT * FROM PointTypes")
         next_entry = self.db.fetchone()

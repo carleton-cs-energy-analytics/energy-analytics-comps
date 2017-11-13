9c9b20b50b48fb46cf359607c8983e9ad66805ed
Carolyn Ryan <ryanc2@carleton.edu>
2017-11-12 19:37:50 -0600

    Try except

--- src/datareaders/siemens/siemens_reader.py
+++ src/datareaders/siemens/siemens_reader.py
@@ -133,8 +133,11 @@ class SiemensReader:
             date = row[1][0]
             time = row[1][0]
             for point in self.points:
-                formatted_value = self._format_value(point, row[1][point.name])
-                self.db_connection.add_point_value(timestamp =  time, point = point, value = formatted_value)
+                try:
+                    formatted_value = self._format_value(point, row[1][point.name])
+                    self.db_connection.add_point_value(timestamp =  time, point = point, value = formatted_value)
+                except ValueError as e:
+                    print("point {} failed to go in with value {}\n".format(point.name, row[1][point.name]))
 
     def _format_value(self, point, raw_value):
         if point.point_type.return_type == "enumerated":
